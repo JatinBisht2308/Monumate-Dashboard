daa85e195d13f3620ab198f0f5de17585b4e9439
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-25 01:25:17 +0530

    :fire: created the readme.md file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Sidebar from './components/Sidebar'
 import Dashboard from './components/Dashboard'
 
 function App() {
-  return (
+  return ( 
     <Div>
         <Sidebar />
         <Dashboard />
